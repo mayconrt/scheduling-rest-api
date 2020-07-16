cd546246975231a7fc1dd28472c0d89a48e6b48f
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2020-07-16 17:55:15 -0300

    Conection Prodution

--- knexfile.js
+++ knexfile.js
@@ -2,53 +2,50 @@
 
 module.exports = {
 
+  // development: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'scheduling',
+  //     user: 'postgres',
+  //     password: 'maycon123'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./src/database/migrations"
+  //   },
+  //   useNullAsDefault: true,
+  // },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
   development: {
-    client: 'postgresql',
-    connection: {
-      database: 'scheduling',
-      user:     'postgres',
-      password: 'maycon123'
-    },
+    client: 'mysql',
+    connection: 'mysql://bf868737566ef5:557e3fec@us-cdbr-east-02.cleardb.com/heroku_2578a9e036304ad?reconnect=true',
     pool: {
       min: 2,
       max: 10
     },
-    migrations:{
+    migrations: {
       directory: "./src/database/migrations"
     },
-    useNullAsDefault:true,    
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true,
   }
 
-};
+}
